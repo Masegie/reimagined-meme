e6afdc8d4908150a246dd790e2b9333446a91af2
Masegie Handoyo Putra <masegihandoyo@gmail.com>
2023-02-24 15:34:14 +0700

    tes commit

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ const people = [
       "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
-    name: "Ted Fox",
+    name: "Ted Fox Led",
     email: "ted.fox@example.com",
     image:
       "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
