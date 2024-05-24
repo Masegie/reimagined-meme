b6c659b3de77a38e5d491affa0b482ff7454589f
Masegie Handoyo Putra <masegihandoyo@gmail.com>
2024-05-24 22:18:14 +0700

    fix button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -201,7 +201,9 @@
     </div>
 
     <!-- Button -->
-    <div class="flex lg:basis-2/12 justify-center">
+    <div
+      class="pointer-events-none bottom-0 left-0 flex lg:basis-2/12 justify-center"
+    >
       <div
         class="p-4 lg:flex-col flex lg:gap-4 justify-items-center lg:items-center justify-center lg:text-xl sm:mt-4 text-sm sm:text-lg"
       >
