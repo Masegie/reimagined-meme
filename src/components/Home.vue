933f20d61e02939297a46ce66e1d8825ca144902
Masegie Handoyo Putra <masegihandoyo@gmail.com>
2024-08-04 16:03:19 +0700

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -825,7 +825,6 @@
                     type="audio/mpeg"
                   />
                 </audio>
-
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 19 19"
