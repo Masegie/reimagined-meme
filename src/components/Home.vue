cdae6a02798f3859418b6791d88c4268c4e2dc16
Masegie Handoyo Putra <masegihandoyo@gmail.com>
2024-08-11 13:37:06 +0700

    change @

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -320,9 +320,9 @@
             v-if="isMeActive === true"
             class="flex mt-0 mb-4 text-blueMarine rounded-2xl sm:rounded-3xl bg-darkGrey lg:p-5 p-4 w-16 h-16 sm:p-6 sm:w-20 sm:h-20 border-4 border-grey50 shadow-[inset_3px_3px_4px_rgba(0,0,0,0.25),inset_-3px_-3px_9px_rgba(255,255,255,0.04)]"
           >
-            <audio ref="typingMusic" loop>
+            <!-- <audio ref="typingMusic" loop>
               <source src="./src/assets/typingEffect.mp3" type="audio/mpeg" />
-            </audio>
+            </audio> -->
 
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -425,9 +425,9 @@
             v-if="isMyWorkActive === false"
             class="flex mt-0 mb-4 text-grey20 rounded-2xl sm:rounded-3xl bg-darkGrey p-4 w-16 h-16 sm:p-6 sm:w-20 sm:h-20 border-4 border-grey50 drop-shadow-button shadow-[inset_-2px_-2px_4px_rgba(0,0,0,0.25),inset_2px_2px_9px_rgba(255,255,255,0.05)] hover:text-grey5 hover:bg-grey70"
           >
-            <audio ref="typingMusic" loop>
+            <!-- <audio ref="typingMusic" loop>
               <source src="./src/assets/typingEffect.mp3" type="audio/mpeg" />
-            </audio>
+            </audio> -->
 
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -518,9 +518,9 @@
             v-if="isMyWorkActive === true"
             class="flex mt-0 mb-4 text-blueMarine rounded-2xl sm:rounded-3xl bg-darkGrey p-4 w-16 h-16 sm:p-6 sm:w-20 sm:h-20 border-4 border-grey50 shadow-[inset_3px_3px_4px_rgba(0,0,0,0.25),inset_-3px_-3px_9px_rgba(255,255,255,0.04)]"
           >
-            <audio ref="typingMusic" loop>
+            <!-- <audio ref="typingMusic" loop>
               <source src="./src/assets/typingEffect.mp3" type="audio/mpeg" />
-            </audio>
+            </audio> -->
 
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -626,9 +626,9 @@
             v-if="isMyContactActive === false"
             class="flex mt-0 mb-4 text-grey20 rounded-2xl sm:rounded-3xl bg-darkGrey p-4 w-16 h-16 sm:p-6 sm:w-20 sm:h-20 border-4 border-grey50 drop-shadow-button shadow-[inset_-2px_-2px_4px_rgba(0,0,0,0.25),inset_2px_2px_9px_rgba(255,255,255,0.05)] hover:text-grey5 hover:bg-grey70"
           >
-            <audio ref="typingMusic" loop>
+            <!-- <audio ref="typingMusic" loop>
               <source src="./src/assets/typingEffect.mp3" type="audio/mpeg" />
-            </audio>
+            </audio> -->
 
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -714,9 +714,9 @@
             v-if="isMyContactActive === true"
             class="flex mt-0 mb-4 text-blueMarine rounded-2xl sm:rounded-3xl bg-darkGrey p-4 w-16 h-16 sm:p-6 sm:w-20 sm:h-20 border-4 border-grey50 shadow-[inset_3px_3px_4px_rgba(0,0,0,0.25),inset_-3px_-3px_9px_rgba(255,255,255,0.04)]"
           >
-            <audio ref="typingMusic" loop>
+            <!-- <audio ref="typingMusic" loop>
               <source src="./src/assets/typingEffect.mp3" type="audio/mpeg" />
-            </audio>
+            </audio> -->
 
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -819,12 +819,12 @@
                 @click="play"
                 class="flex mt-0 mb-4 redCVHover rounded-full bg-darkGrey p-4 w-16 h-16 sm:p-6 sm:w-20 sm:h-20 border-4 border-grey50 drop-shadow-button active:drop-shadow-sm shadow-[inset_-2px_-2px_4px_rgba(0,0,0,0.25),inset_2px_2px_9px_rgba(255,255,255,0.05)] hover:text-redCVHover hover:bg-grey70 transition transform duration-150 active:shadow-[inset_3px_3px_4px_rgba(0,0,0,0.25),inset_-3px_-3px_9px_rgba(255,255,255,0.04)] active:text-grey50"
               >
-                <audio ref="typingMusic" loop>
+                <!-- <audio ref="typingMusic" loop>
                   <source
                     src="./src/assets/typingEffect.mp3"
                     type="audio/mpeg"
                   />
-                </audio>
+                </audio> -->
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 19 19"
